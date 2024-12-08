a6e133e00261913eed7468da39d72181dd119f09
adilraees786 <adilraees911@gmail.com>
2024-12-08 23:40:26 +0500

    working on home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Navbar } from "./components/Navbar";
 export default function App() {
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Home/>
     </div>
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import myimage from "../assets/assets/myimage.jpg"
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
  const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <div>
+            <h2>
+                I'm a Full Stack Developer
+            </h2>
+            <p> I have 1 Year experience building and
+                designing softwear.
+                Currently, I love to work on web application
+                using technologies like
+                React, Next js, Tailwind, CSS, Html </p>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Home;
