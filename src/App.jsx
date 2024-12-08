3c1bfb2339fca01ee388a58156702b2c53c3598b
adilraees786 <adilraees911@gmail.com>
2024-12-08 22:14:42 +0500

    done complete setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold  text-red-700">
+    <h1 className="text-3xl font-bold  font-signature text-red-700">
       My Portfolio
 
     </h1>
