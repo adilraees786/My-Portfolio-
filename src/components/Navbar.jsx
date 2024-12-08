60f93ead0b00167a9cbdd59d09aa09b3abaae20e
adilraees786 <adilraees911@gmail.com>
2024-12-08 22:41:22 +0500

    complete Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,51 @@
 import React from 'react'
 
 export const Navbar = () => {
+const links=[
+    {
+       id: 1,
+       link: 'home'
+    },
+    {
+       id: 2,
+       link: 'about'
+    },
+    {
+       id: 3,
+       link: 'portfolio'
+    },
+    {
+       id: 4,
+       link: 'experience'
+    },
+    {
+       id: 5,
+       link: 'contact'
+    },
+]
+
   return (
     <div className='flex justify-between items-center w-full h-20
      text-white bg-black  px-4 fixed '>
         <div>
             <h1 className=' text-5xl font-signature ml-2'>Adil Raees</h1>
         </div>
+<ul className=' flex'>
 
+{links.map (({id, link}) =>(
+     <li
+     key={id}
+      className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
+        {link}
+      </li>
+
+))}
+
+   
+   
+
+    
+</ul>
     </div>
   )
 }
