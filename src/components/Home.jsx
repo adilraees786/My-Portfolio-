b87d6098367f832025b515e9ef4bd59a77b108a0
adilraees786 <adilraees911@gmail.com>
2024-12-09 15:12:53 +0500

    continue home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import myimage from "../assets/assets/myimage.jpg"
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
- const Home = () => {
+const Home = () => {
   return (
     <div>
       <div>
         <div>
-            <h2>
-                I'm a Full Stack Developer
-            </h2>
-            <p> I have 1 Year experience building and
-                designing softwear.
-                Currently, I love to work on web application
-                using technologies like
-                React, Next js, Tailwind, CSS, Html </p>
+          <h2>
+            I'm a Full Stack Developer
+          </h2>
+          <p> I have 1 Year experience building and
+            designing softwear.
+            Currently, I love to work on web application
+            using technologies like
+            React, Next js, Tailwind CSS, Html. </p>
+
+            <div>
+              <button>Portfolio</button>
+            </div>
         </div>
+
       </div>
     </div>
   )
