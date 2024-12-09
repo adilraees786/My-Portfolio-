bf1fe8fe3f3dd0fab55bf6d860ac2feb55df7aec
adilraees786 <adilraees911@gmail.com>
2024-12-09 15:35:08 +0500

    bg home set

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,28 +4,32 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div name="home" className=' h-screen  w-full bg-gradient-to-b from-black to-gray-800 '>
+    <div name="home" className=' h-screen  w-full bg-gradient-to-b from-black via-black to-gray-800 '>
       <div className=' max-w-screen-lg mx-auto flex flex-col
-      items-center justify-center h-full'>
-        <div>
-          <h2>
+      items-center justify-center h-full px-4 md:flex-row'>
+        <div className=' flex flex-col justify-center h-full'>
+          <h2 className=' text-4xl sm:text-7xl font-bold text-white'>
             I'm a Full Stack Developer
           </h2>
-          <p> I have 1 Year experience building and
+          <p className='  text-gray-500 py-4 max-w-md'> I have 1 Year experience building and
             designing softwear.
             Currently, I love to work on web application
             using technologies like
             React, Next js, Tailwind CSS, Html. </p>
 
-            <div>
-              <button>Portfolio
-                <span>
-                <MdOutlineKeyboardArrowRight/>
-                </span>
-              </button>
-            </div>
+          <div>
+            <button className=' '>
+              Portfolio
+              <span>
+                <MdOutlineKeyboardArrowRight />
+              </span>
+            </button>
+          </div>
         </div>
 
+        <div>
+          <img src={myimage} alt=" my profile" className=' rounded-2xl mx-auto w-2/3 md:w-full' />
+        </div>
       </div>
     </div>
   )
