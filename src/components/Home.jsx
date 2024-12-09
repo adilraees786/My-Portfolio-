3adbce06963c5a6dd6aa0026a35403a32e704e30
adilraees786 <adilraees911@gmail.com>
2024-12-09 15:38:10 +0500

    set portfolio button in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,9 @@ const Home = () => {
             React, Next js, Tailwind CSS, Html. </p>
 
           <div>
-            <button className=' '>
+            <button className=' text-white w-fit px-6
+             py-3 my-2 flex items-center rounded-md bg-gradient-to-r 
+             from-cyan-500 to-blue-500 cursor-pointer '>
               Portfolio
               <span>
                 <MdOutlineKeyboardArrowRight />
