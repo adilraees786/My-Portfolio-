1f48224cf4abf8e7572be4b08d6c722e0d17e97c
adilraees786 <adilraees911@gmail.com>
2024-12-09 15:20:01 +0500

    add style on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,9 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div>
-      <div>
+    <div name="home" className=' h-screen  w-full bg-gradient-to-b from-black to-gray-800 '>
+      <div className=' max-w-screen-lg mx-auto flex flex-col
+      items-center justify-center h-full'>
         <div>
           <h2>
             I'm a Full Stack Developer
@@ -17,7 +18,11 @@ const Home = () => {
             React, Next js, Tailwind CSS, Html. </p>
 
             <div>
-              <button>Portfolio</button>
+              <button>Portfolio
+                <span>
+                <MdOutlineKeyboardArrowRight/>
+                </span>
+              </button>
             </div>
         </div>
 
