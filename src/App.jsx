a61b59c72a819624cdea1e517a2e5851a5bffb1a
adilraees786 <adilraees911@gmail.com>
2024-12-11 16:20:46 +0500

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { About } from "./components/About";
 import Home from "./components/Home";
 import { Navbar } from "./components/Navbar";
 import Sociallinks from "./components/Sociallinks";
@@ -9,6 +10,7 @@ export default function App() {
     <div>
       <Navbar/>
       <Home/>
+      <About/>
 <Sociallinks/>
       
     </div>
