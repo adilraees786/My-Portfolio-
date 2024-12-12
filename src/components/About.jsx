824c74b67e1eb2cd674dece73a26c6c2d47836d6
adilraees786 <adilraees911@gmail.com>
2024-12-12 14:16:05 +0500

    add style in about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,13 @@ const About = () => {
   return (
     <div name="about" className=' w-full h-screen bg-gradient-to-b from-gray-800
     to-black text-white'>
-      <div>
-        <div>
-          <p> About</p>
+      <div className=' max-w-screen-lg p-4 mx-auto flex flex-col
+      justify-center w-full h-full'>
+        <div className='pb-8'>
+          <h1 className=' text-4xl font-bold inline border-b-4 
+          border-gray-500'> 
+          About
+          </h1>
         </div>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo totam necessitatibus expedita voluptate cum,
            quia tenetur suscipit neque commodi? Quia et facilis soluta debitis iure! Cum omnis quaerat blanditiis odio
