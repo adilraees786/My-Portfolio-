c556417168647b9445e6c9803487abc3b5d87af5
adilraees786 <adilraees911@gmail.com>
2024-12-12 14:07:31 +0500

    continue About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
-export const About = () => {
+const About = () => {
   return (
-    <div>About page</div>
+    <div>
+      <div>
+        <div>
+          <p> About</p>
+        </div>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo totam necessitatibus expedita voluptate cum,
+           quia tenetur suscipit neque commodi? Quia et facilis soluta debitis iure! Cum omnis quaerat blanditiis odio
+            esse optio consequatur asperiores fuga hic vel placeat eius saepe id quae, velit assumenda unde sit,
+             obcaecati quasi eligendi ratione.</p>
+             <br />
+             <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime reiciendis magni perspiciatis expedita 
+              autem reprehenderit quibusdam! Recusandae aspernatur id et est ipsa tempore iusto ratione nulla 
+              consequatur numquam ullam assumenda quibusdam temporibus harum vel quasi magni obcaecati vitae,
+               suscipit voluptate omnis dignissimos amet sapiente maxime. Et repellendus autem ullam at?
+             </p>
+      </div>
+    </div>
   )
 }
+
+export default About;
