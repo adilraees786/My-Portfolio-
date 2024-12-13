a9df9678627df269b99daf0cb7adc2732d229c70
adilraees786 <adilraees911@gmail.com>
2024-12-13 16:13:47 +0500

    add portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { About } from "./components/About";
+import  About  from "./components/About";
 import Home from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 import Sociallinks from "./components/Sociallinks";
 
 
@@ -11,6 +12,7 @@ export default function App() {
       <Navbar/>
       <Home/>
       <About/>
+      <Portfolio/>
 <Sociallinks/>
       
     </div>
