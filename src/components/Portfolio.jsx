8894602f4ee87656ba554e04fac29a2999fe8794
adilraees786 <adilraees911@gmail.com>
2024-12-13 16:15:13 +0500

    add divs in

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div>
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
   )
 }
 export default Portfolio;
