33d64d5aa90e17b0344f52d68cb09126af690078
adilraees786 <adilraees911@gmail.com>
2024-12-14 12:41:40 +0500

    add p tag in portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,6 +6,7 @@ const Portfolio = () => {
         <div>
             <div>
                 <h1>Portfolio</h1>
+                <p>Check out some of my work right here</p>
             </div>
         </div>
     </div>
