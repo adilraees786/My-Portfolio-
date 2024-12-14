0eff13a0c89d3e046a1dcabdc76521e56c29648a
adilraees786 <adilraees911@gmail.com>
2024-12-14 12:47:28 +0500

    add butoon in portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,12 +3,24 @@ import React from 'react'
 const Portfolio = () => {
   return (
     <div>
+      <div>
         <div>
-            <div>
-                <h1>Portfolio</h1>
-                <p>Check out some of my work right here</p>
-            </div>
+          <h1>Portfolio</h1>
+          <p>Check out some of my work right here</p>
         </div>
+        <div>
+
+          <div>
+            <img src="" alt="" />
+            <div>
+              <button>Demo</button>
+              <button>Code</button>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
