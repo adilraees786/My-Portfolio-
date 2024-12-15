ca6b4acc1af508339c0f3bb4251b24ae91310f93
adilraees786 <adilraees911@gmail.com>
2024-12-15 23:11:31 +0500

    set p tag in portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,7 +9,7 @@ const Portfolio = () => {
         <div className=' pb-8'>
           <h1 className=' text-4xl font-bold inline border-b-4
           border-gray-500'>Portfolio</h1>
-          <p>Check out some of my work right here</p>
+          <p className=' py-6'>Check out some of my work right here</p>
         </div>
         <div>
 
