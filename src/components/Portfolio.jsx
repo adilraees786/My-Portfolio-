68e86b1f4544777f158a7426b6b71b1514f906e5
adilraees786 <adilraees911@gmail.com>
2024-12-15 23:57:45 +0500

    add more progects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
-import ecommerce from '../assets/assets/ecommerce.png'
+import ecommerce from '../assets/assets/portfolio/ecommerce.png'
+import todolist from '../assets/assets/portfolio/todolist.png'
+import budgetapp from '../assets/assets/portfolio/budetapp.png'
+import onlinestore from '../assets/assets/portfolio/onlinestore.png'
+import randomwords from '../assets/assets/portfolio/randomwords.png'
+import trafficlight from '../assets/assets/portfolio/trafficlight.png'
+import passvalidation from '../assets/assets/portfolio/passvalidation.png'
 
 const Portfolio = () => {
+
+  const portfolios = [
+    {
+      id: 1,
+      src: todolist
+    },
+    {
+      id: 2,
+      src: budgetapp
+    },
+    {
+      id: 3,
+      src: onlinestore
+    },
+    {
+      id: 4,
+      src: randomwords
+    },
+    {
+      id: 5,
+      src: trafficlight
+    },
+    {
+      id: 6,
+      src: passvalidation
+    }
+  ]
   return (
     <div name="portfolio" className=' bg-gradient-to-b from-black to-gray-800 
     w-full text-white md:h-screen'>
@@ -18,8 +51,8 @@ const Portfolio = () => {
           <div className=' shadow-md shadow-gray-600 rounded-lg '>
             <img src={ecommerce} alt=""  className=' rounded-md duration-200 hover:scale-105 '/>
             <div className='flex items-center justify-center'>
-              <button>Demo</button>
-              <button>Code</button>
+              <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+              <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
             </div>
 
           </div>
