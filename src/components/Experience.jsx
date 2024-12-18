181f179b0aae2e09ad64607f818cc583cc52e383
adilraees786 <adilraees911@gmail.com>
2024-12-18 15:09:45 +0500

    import images in experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import javascript from '../assets/ javascript.png'
+import ract from '../assets/react.png'
+import nextjs from '../assetsnextjsl.png'
+import tailwind from '../assets/tailwind.png'
+import github from '../assets/github.png'
+import node from '../assets/node.png'
 
  const Experience = () => {
   return (
