5c570f73a2cfb24d84bac45a2d34bf724541cdc8
adilraees786 <adilraees911@gmail.com>
2024-12-18 16:03:48 +0500

    complete Experiene page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,76 @@
 import React from 'react'
 import html from '../assets/assets/portfolio/html.png';
-// import html from '../assets/assets/portfolio/html.png';
-// import html from '../assets/assets/portfolio/html.png';
-// import html from '../assets/assets/portfolio/html.png';
-// import html from '../assets/assets/portfolio/html.png';
-// import html from '../assets/assets/portfolio/html.png';
-// import html from '../assets/assets/portfolio/html.png';
-// import html from '../assets/assets/portfolio/html.png';
+import css from '../assets/assets/portfolio/css.png';
+import javascript from '../assets/assets/portfolio/javascript.png';
+import react from '../assets/assets/portfolio/react.png';
+import nextjs from '../assets/assets/portfolio/nextjs.png';
+import tailwind from '../assets/assets/portfolio/tailwind.png';
+import node from '../assets/assets/portfolio/node.png';
+import gitgub from '../assets/assets/portfolio/github.png';
 
 const Experience = () => {
+
+const techs =[
+{
+  id: 1,
+  src: 'html',
+  title: "HTML",
+  style: "shadow-orange-500"
+
+},
+{
+  id: 2,
+  src: 'css',
+  title: "CSS",
+  style: "shadow-blue-500"
+
+},
+{
+  id: 3,
+  src: 'javascript',
+  title: "JavaScript",
+  style: "shadow-yellow-500"
+
+},
+
+{
+  id: 4,
+  src: 'react',
+  title: "React",
+  style: "shadow-blue-600"
+
+},
+{
+  id: 5,
+  src: 'tailwind',
+  title: "Tailwind",
+  style: "shadow-sky-400"
+
+},
+{
+  id: 6,
+  src: 'nextjs',
+  title: "Next JS",
+  style: "shadow-white"
+
+},
+{
+  id: 7,
+  src: 'node',
+  title: "Node JS",
+  style: "shadow-white"
+
+},
+{
+  id: 8,
+  src: 'hithub',
+  title: "GitHub",
+  style: "shadow-gray-400"
+
+}
+
+]
+
   return (
     <div name='experience' className=' bg-gradient-to-b from-gray-800 to-black
     w-full h-screen'>
@@ -24,7 +86,7 @@ const Experience = () => {
         gap-8 text-center py-8 px-12 sm:px-0'>
           <div className=' shadow-md hover:scale-105 duration-500
           py-2 rounded-lg'>
-            <img src={html} alt="" />
+            <img src={html}  alt="" className="w-20 mx-auto" />
             <p>HTML</p>
           </div>
         </div>
