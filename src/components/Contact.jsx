b7ae456e3cdcf53ab50d8d8f2fc93cb81403372e
adilraees786 <adilraees911@gmail.com>
2024-12-19 12:27:53 +0500

    add styling in Contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
  const Contact = () => {
   return (
-    <div>
+    <div name="contact" className=' w-full  h-screen
+    bg-gradient-to-b from-black to-gray-800 p-4
+    text-white'>
 
-       <div>
-        <div>
-            <h1>Contact</h1>
+       <div className=' flex flex-col p-4 justify-center
+       max-w-screen-lg mx-auto h-full'>
+        <div className=' pb-8'>
+            <h1 className=' text-4xl font-bold inline border-b-4
+            border-gray-500'>
+                Contact</h1>
+                
             <p>Submit the from to get in touch with me</p>
         </div>
 <div>
@@ -20,6 +26,15 @@ import React from 'react'
          placeholder="Enter your Email"
          className=' p-2 bg-transparent border-2 rounded-md
          text-white focus:outline-none' />
+
+         <textarea name="message" rows="10" className=' p-2
+        bg-transparent border-2 rounded-md text-white focus:outline-none '>
+         </textarea>
+
+         <button className=' text-white bg-gradient-to-b from-cyan-500
+         to-blue-500 px-6 py-3 my-8 mx-auto
+         flex items-center rounded-md hover:scale-110 duration-300'>
+            Let's talk</button>
 
     </form>
 </div>
