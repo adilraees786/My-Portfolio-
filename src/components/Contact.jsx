2ae07d345b9cd60fe8620fd25aac7b4fc7ef3800
adilraees786 <adilraees911@gmail.com>
2024-12-19 12:14:13 +0500

    Enter contact details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,27 @@ import React from 'react'
  const Contact = () => {
   return (
     <div>
+
        <div>
         <div>
             <h1>Contact</h1>
             <p>Submit the from to get in touch with me</p>
         </div>
+<div>
+    <form action="">
+        <input type="text" name="name"
+         placeholder="Enter your Name"
+         className=' p-2 bg-transparent border-2 rounded-md
+         text-white focus:outline-none' />
+
+        <input type="email" name="email"
+         placeholder="Enter your Email"
+         className=' p-2 bg-transparent border-2 rounded-md
+         text-white focus:outline-none' />
+
+    </form>
+</div>
+
         </div> 
     </div>
   )
