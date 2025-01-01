6568de42b82b1874cac32f46a13ec5b37c2b4a0b
adilraees786 <adilraees911@gmail.com>
2025-01-01 13:52:22 +0500

    add fontweight bold

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
           </h1>
         </div>
         <p className=' text-xl mt-20'>I am a student at the University of FUUAST with a major in 
-           <span className=' text-blue-900'> Computer Science</span> . As a skilled MERN Stack Developer with a solid foundation in React.js,
+           <span className=' text-blue-900 font-extrabold' > Computer Science</span> . As a skilled MERN Stack Developer with a solid foundation in React.js,
             Next.js, React Native, Tailwind CSS  and Firebase, I specialize in crafting dynamic, responsive 
             Web and App Applications that solve real-world problems. With experience in full-stack
              development, I am passionate about delivering robust and scalable solutions.</p>
@@ -22,7 +22,7 @@ const About = () => {
         Alongside my technical skills, I am actively engaged in social media activism, where I
          blend my development expertise with community engagement to create impactful digital
           platforms. I am committed to continuous learning and currently advancing my skills
-           in JavaScript frameworks and modern web technologies.
+           in  <span className=''> JavaScript</span> frameworks and modern web technologies.
         </p>
       </div>
     </div>
