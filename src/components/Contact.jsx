bd72afc083edf4b4e46f4c58a9c7c0fe2694a304
adilraees786 <adilraees911@gmail.com>
2025-01-09 20:18:47 +0500

    set issues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,56 +2,59 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div name="contact" className=' w-full  h-screen
-    bg-gradient-to-b from-black to-gray-800 p-4
-    text-white'>
+        <div
+            className='w-full h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white'
+            name="contact"
+        >
+            <div className=' flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
 
-            <div className=' flex flex-col p-4 justify-center
-       max-w-screen-lg mx-auto h-full'>
                 <div className=' pb-8'>
-                    <h1 className=' text-4xl font-bold inline border-b-4
-            border-gray-500'>
-                        Contact</h1>
-
-                    <p className=' py-6'>Submit the from to get in touch with me</p>
+                    <h1 className=' text-4xl font-bold inline border-b-4 border-gray-500'>
+                        Contact
+                    </h1>
+                    <p className='py-6'>Submit the from to get in touch with me</p>
                 </div>
-                <div className=' flex justify-center items-center'>
 
-                    <form action="https://getform.io/f/bnllwdwb" method='POST' className=' flex flex-col w-full md:w-1/2'>
-                        <input type="text" name="name"
+                <div className=' flex justify-center items-center'>
+                    <form
+                        action="https://getform.io/f/bnllwdwb"
+                        method='POST'
+                        className=' flex flex-col w-full md:w-1/2'
+                    >
+
+                        <input
+                            type="text"
+                            name="name"
                             placeholder="Enter your Name"
                             required
-                            className=' p-2 bg-transparent border-2 rounded-md
-         text-white focus:outline-none' />
+                            className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+                        />
 
-                        <input type="email" name="email"
+                        <input
+                            type="email"
+                            name="email"
                             placeholder="Enter your Email"
                             required
-                            className=' my-4 p-2 bg-transparent border-2 rounded-md
-         text-white focus:outline-none' />
+                            className=' my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+                        />
 
-                        <textarea name="message" rows="10"
+                        <textarea
+                            name="message"
+                            rows="10"
                             placeholder="Enter your Meassage"
                             required
-                            className=' p-2
-        bg-transparent border-2 rounded-md text-white focus:outline-none '>
+                            className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none '>
                         </textarea>
 
-                        <button className=' text-white bg-gradient-to-b from-cyan-500
-         to-blue-500 px-6 py-3 my-8 mx-auto
-         flex items-center rounded-md hover:scale-110 duration-300'>
-                            Let's talk</button>
-
+                        <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>
+                            Let's talk
+                        </button>
                     </form>
                 </div>
-
             </div>
         </div>
     )
-}
+};
+
+
 export default Contact;
-
-
-
-
-
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,92 +1,88 @@
 import React from 'react'
-import { FaFacebook, FaGithub, FaInstagram, FaLinkedin} from "react-icons/fa";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 
- const SocialLinks = () => {
+const SocialLinks = () => {
 
-const links=[
-  {
-    id: 1,
-    child:(
-      <>
-      linkedin <FaLinkedin size={30}/>
-      </>
-    ),
-    href: "https://www.linkedin.com/in/adil-raees-08436a2b2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    style: "rounded-tr-md"
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          linkedin <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/adil-raees-08436a2b2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      style: "rounded-tr-md"
 
-  },
-  {
-    id: 2,
-    child:(
-      <>
-      Github <FaGithub size={30}/>
-      </>
-    ),
-    href: "https://github.com/adilraees786",
-    style: "rounded-tr-md"
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          Github <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/adilraees786",
+      style: "rounded-tr-md"
 
-  },
-  {
-    id: 3,
-    child:(
-      <>
-      Instagram <FaInstagram size={30}/>
-      </>
-    ),
-    href: "https://www.instagram.com/adii_hehe?igsh=MTFpYnNoaGV0enB5cg==",
-    style: "rounded-tr-md"
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Instagram <FaInstagram size={30} />
+        </>
+      ),
+      href: "https://www.instagram.com/adii_hehe?igsh=MTFpYnNoaGV0enB5cg==",
+      style: "rounded-tr-md"
 
-  },
-  {
-    id: 4,
-    child: (
-      <>
-        Mail <SiGmail size={30} />
-      </>
-    ),
-    href: "mailto:adilraees911@gmail.com",
-    style: "rounded-tr-md",
-  },
-  {
-    id: 5,
-    child:(
-      <>
-      Facebook <FaFacebook size={30}/>
-      </>
-    ),
-    href: "https://www.facebook.com/adil.raees.96?mibextid=ZbWKwL",
-    style: "rounded-tr-md"
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Mail <SiGmail size={30} />
+        </>
+      ),
+      href: "mailto:adilraees911@gmail.com",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 5,
+      child: (
+        <>
+          Facebook <FaFacebook size={30} />
+        </>
+      ),
+      href: "https://www.facebook.com/adil.raees.96?mibextid=ZbWKwL",
+      style: "rounded-tr-md"
 
-  },
-]
+    },
+  ]
 
   return (
     <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        
+
         {links.map(({ id, child, href, style }) => (
-  <li
-    key={id}
-    className={
-      'flex justify-between items-center w-40  h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + " " +
-      style
-    }
-  >
-    <a
-      href={href}
-      className="flex justify-between items-center w-full text-white"
-    >
-      {child}
-    </a>
-  </li>
-))}
+          <li
+            key={id}
+            className={
+              'flex justify-between items-center w-40  h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
 
-
-        
-            
       </ul>
-   
     </div>
   )
 }
