ef0d25726884f1bb54f4076958c8f54a118cebee
adilraees786 <adilraees911@gmail.com>
2025-01-10 14:38:43 +0500

    Add mern stack in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       items-center justify-center h-full px-4 md:flex-row'>
         <div className=' flex flex-col justify-center h-full'>
           <h2 className=' text-4xl sm:text-7xl font-bold text-white'>
-            I'm a Full Stack Developer
+            I'm a MERN Stack Developer
           </h2>
           <p className='  text-gray-500 py-4 max-w-md'> I have  <span className=' font-extrabold'>1</span> Year experience building and
             designing softwear.
